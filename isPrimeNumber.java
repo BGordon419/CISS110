7fb233b6da892c83b93b043f637e36ac85c7d19b
Brandon Gordon <BGordon419@gmail.com>
2014-11-19 09:50:50 -0500

    small modifications

--- isPrimeNumber.java
+++ isPrimeNumber.java
@@ -1,29 +1,24 @@
 import javax.swing.JOptionPane;
 
-public class isPrimeNumber
-{
-     
-     public static void main(String[] args)
-     {
+public class isPrimeNumber {
+
+     public static void main(String[] args) {
           // Inputs
           String number = JOptionPane.showInputDialog("Enter a number.");
-          
+
           // Outputs - sends user input to 'isPrime' Method
           JOptionPane.showMessageDialog(null,isPrime(Integer.parseInt(number)));
      }
-     
+
      // Modified from http://en.wikipedia.org/wiki/Primality_test#Java_implementation
-     public static boolean isPrime(int n)
-     {
-          
+     public static boolean isPrime(int n) {
+
           if(n <= 3 && n > 1)
                return true;
           else if(n % 2 == 0 || n % 3 == 0 || n == 1)
                return false;
-          else
-          {
-               for(int x = 5; x * x <= n; x += 6)
-               {
+          else {
+               for(int x = 5; x * x <= n; x += 6) {
                     if(n % x == 0 || n % (x + 2) == 0)
                          return false;
                }
